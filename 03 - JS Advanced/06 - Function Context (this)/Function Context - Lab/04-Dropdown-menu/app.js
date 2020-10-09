1b922a59b102641a3267c243674e3127f4c109cc
Iva Tosheva <otevi55@gmail.com>
2020-10-09 22:15:29 +0300

    100/100

--- 03 - JS Advanced/06 - Function Context (this)/Function Context - Lab/04-Dropdown-menu/app.js
+++ 03 - JS Advanced/06 - Function Context (this)/Function Context - Lab/04-Dropdown-menu/app.js
@@ -1,3 +1,27 @@
 function solve() {
-    // ToDo
+    let button = document.querySelector("#dropdown");
+    let dropdownUl = document.querySelector("#dropdown-ul");
+    let box = document.querySelector("#box");
+   
+    [...document.querySelectorAll("li")].forEach(li => {
+        li.addEventListener("click", changeBoxColor)
+    });
+
+    button.addEventListener("click", changeStyle);
+
+    function changeStyle(e) {
+        if (dropdownUl.style.display != "block") {
+            dropdownUl.style.display = "block";
+        } else {
+            dropdownUl.style.display = "none";
+            box.style.backgroundColor = "black";
+            box.style.color = "white";
+        }
+    }
+
+    function changeBoxColor(e) {
+        let tempColor = e.target.textContent;
+        box.style.backgroundColor = tempColor;
+        box.style.color = 'black';
+    }
 }
