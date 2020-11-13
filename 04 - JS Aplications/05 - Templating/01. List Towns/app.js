1a0a0cd4afc42cc01371a016c0de17a42eb76373
Iva Tosheva <otevi55@gmail.com>
2020-11-13 23:05:05 +0200

    updated

--- 04 - JS Aplications/05 - Templating/01. List Towns/app.js
+++ 04 - JS Aplications/05 - Templating/01. List Towns/app.js
@@ -6,12 +6,15 @@ window.addEventListener("load", () => {
     function addToList(e) {
         e.preventDefault();
         const towns = townInput.value.split(", ");
-        const view = document.querySelector("#list-template").innerHTML;
-        const template = Handlebars.compile(view);
+       
+        if (towns.length > 1) {
+            const view = document.querySelector("#list-template").innerHTML;
+            const template = Handlebars.compile(view);
 
-        const generateHtml = template({towns});
-        rootDiv.innerHTML = generateHtml;
+            const generateHtml = template({ towns });
+            rootDiv.innerHTML = generateHtml;
 
-        townInput.value = "";
+            townInput.value = "";
+        }
     }
 });
