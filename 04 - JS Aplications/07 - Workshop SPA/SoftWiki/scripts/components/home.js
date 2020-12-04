c201e2998a542a25433f4c08892412c8f41b9f2f
Iva Tosheva <otevi55@gmail.com>
2020-12-04 15:55:26 +0200

    implemented home page

--- 04 - JS Aplications/07 - Workshop SPA/SoftWiki/scripts/components/home.js
+++ 04 - JS Aplications/07 - Workshop SPA/SoftWiki/scripts/components/home.js
@@ -1,26 +1,7 @@
 import { getAllData, loadPage, mapCategories, errorHandler } from '../helpers.js'
 
 export async function home() {
-    // getAllData()
-    //     .then(res => {
-    //         this.posts = Object.entries(res)
-    //             .map(([postId, data]) => { return { postId, ...data } })
-    //         changeContext(this);
-    //         loadPage.call(this, 'homePage');
-    //     }).catch( err => {
-    //         changeContext(this);
-    //         loadPage.call(this, 'homePage');
-    //         errorHandler(err)
-    //     })
-       // console.log(await getAllData())
-        this.articles = mapCategories(await getAllData());
-        console.log(this.articles)
-        // let articles = Object.entries(data)
-        //     .map(([articleId, data]) => { return { articleId, ...data } })
-
-        
-        // console.log(articles.filter(x => x.category))
-   
+    this.articles = mapCategories(await getAllData());
     changeContext(this);
-        loadPage.call(this, 'homePage');
+    loadPage.call(this, 'homePage');
 }
