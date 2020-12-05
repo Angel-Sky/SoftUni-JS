eff08114e313976568be2a0d5685c66c57d2a7a5
Iva Tosheva <otevi55@gmail.com>
2020-12-05 15:01:14 +0200

    project configuration

---
+++ 04 - JS Aplications/07 - Workshop SPA/SoftTerest/scripts/config.js
@@ -0,0 +1,25 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBwy465BbLlHh06SFtW2K9EhwFXL3l8b-Q",
+    authDomain: "my-blog-softuni.firebaseapp.com",
+    databaseURL: "https://my-blog-softuni.firebaseio.com",
+    projectId: "my-blog-softuni",
+    storageBucket: "my-blog-softuni.appspot.com",
+    messagingSenderId: "21874307407",
+    appId: "1:21874307407:web:e7652b3d22431bd252d53a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const baseUrl =  firebaseConfig.databaseURL + '/ideas';
+
+function changeContext(context) {
+    let user = sessionStorage.getItem("user");
+    if (user) {
+        context.isLoggedIn = true;
+        context.email = user.email;
+        context.userId = user.localId;
+    } else {
+        context.isLoggedIn = false;
+    }
+}
