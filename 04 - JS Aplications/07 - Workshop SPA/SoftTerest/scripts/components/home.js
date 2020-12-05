e0dbcbb76b39affe19408a78bd3caf3c32c3c462
Iva Tosheva <otevi55@gmail.com>
2020-12-05 16:06:14 +0200

    Home page displays all created ideas

--- 04 - JS Aplications/07 - Workshop SPA/SoftTerest/scripts/components/home.js
+++ 04 - JS Aplications/07 - Workshop SPA/SoftTerest/scripts/components/home.js
@@ -1,7 +1,15 @@
-import { getAllData, loadPage, mapCategories } from '../helpers.js'
+import { getAllData, loadPage, mapCategories, errorHandler } from '../helpers.js'
 
 export async function home() {
     //TO DO
+    getAllData()
+        .then(res => {
+            this.ideas = Object.entries(res)
+                .map(([id, data]) => { return { id, ...data } })
+            changeContext(this);
+            loadPage.call(this, 'homePage', 'idea');
+        }).catch(errorHandler);
+
     changeContext(this);
-    loadPage.call(this, 'homePage');
+    loadPage.call(this, 'homePage', 'idea');
 }
