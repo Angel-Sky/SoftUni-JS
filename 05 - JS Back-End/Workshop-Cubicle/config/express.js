848c46d6c3e5efe7fb983f325c08acad7278e2ea
Iva Tosheva <otevi55@gmail.com>
2021-02-11 15:50:34 +0200

    extract express config

--- 05 - JS Back-End/Workshop-Cubicle/config/express.js
+++ 05 - JS Back-End/Workshop-Cubicle/config/express.js
@@ -3,7 +3,13 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 module.exports = (app) => {
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
     
+    app.set('view engine', 'hbs');
+    
+    app.use(express.static("static"));
     //TODO: Setup the view engine
 
     //TODO: Setup the body parser
--- 05 - JS Back-End/Workshop-Cubicle/index.js
+++ 05 - JS Back-End/Workshop-Cubicle/index.js
@@ -1,17 +1,10 @@
 const config = require('./config/config');
-const handlebars = require('express-handlebars');
 const express = require('express');
 const app = express()
 
-// require('./config/express')(app);
+require('./config/express')(app);
 // require('./config/routes')(app);
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}));
 
-app.set('view engine', 'hbs');
-
-app.use(express.static("static"))
 app.get('/', (req, res) => {
     res.render('home', {layout: false})
 })
