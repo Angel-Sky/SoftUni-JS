b3fb9b15c00c7fdd64f476843edf5f01f0b7f8ac
Iva Tosheva <otevi55@gmail.com>
2021-02-12 14:11:16 +0200

    mongoose connected

--- 05 - JS Back-End/Workshop-Cubicle/config/express.js
+++ 05 - JS Back-End/Workshop-Cubicle/config/express.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
 
 module.exports = (app) => {
     app.engine('hbs', handlebars({
         extname: 'hbs'
     }));
-    
+
     app.set('view engine', 'hbs');
-    
+
     app.use(express.static("static"));
     app.use(express.urlencoded({
         extended: true
     }));
-    //TODO: Setup the view engine
-
-    //TODO: Setup the body parser
-
-    //TODO: Setup the static files
-
 };
--- 05 - JS Back-End/Workshop-Cubicle/index.js
+++ 05 - JS Back-End/Workshop-Cubicle/index.js
@@ -4,6 +4,7 @@ const app = express();
 const routes = require('./config/routes');
 
 require('./config/express')(app);
+require('./config/mongoose')(app);
 
 app.use(routes);
 app.listen(config.PORT, console.log(`Listening on port ${config.PORT}...`));
