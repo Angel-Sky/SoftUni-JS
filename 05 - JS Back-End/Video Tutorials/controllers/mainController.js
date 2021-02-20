511f432ef7d7ffdd7911c4bf36a4c09976c831fc
Iva Tosheva <otevi55@gmail.com>
2021-02-20 21:57:12 +0200

    Update mainController.js

--- 05 - JS Back-End/Video Tutorials/controllers/mainController.js
+++ 05 - JS Back-End/Video Tutorials/controllers/mainController.js
@@ -3,15 +3,19 @@ const courseService = require('../services/courseService');
 const moment = require("moment");
 
 router.get('/', async (req, res) => {
-    let courses;
-    if (req.user) {
-        courses = await courseService.getAll(req.query.search);
-        courses = courses.map(x => ({...x, createdAt: moment(x.createdAt).format('ddd MMM d YYYY HH:mm:ss')}))
-    } else {
-        courses = await courseService.getMostPop(3);
-    }
+    try {
+        let courses;
+        if (req.user) {
+            courses = await courseService.getAll(req.query.search);
+            courses = courses.map(x => ({ ...x, createdAt: moment(x.createdAt).format('ddd MMM d YYYY HH:mm:ss') }))
+        } else {
+            courses = await courseService.getMostPop(3);
+        }
 
-    res.render('home', { title: 'Video Tutorials', courses })
+        res.render('home', { title: 'Video Tutorials', courses })
+    } catch (error) {
+        res.render('home', { title: 'Video Tutorials', courses, error })
+    }
 });
 
 
