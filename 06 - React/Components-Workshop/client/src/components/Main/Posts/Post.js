3a7b4e3224983059c2079c00960c87af9440573a
Iva Tosheva <otevi55@gmail.com>
2021-03-15 13:52:22 +0200

    add post component

--- 06 - React/Components-Workshop/client/src/components/Main/Posts/Post.js
+++ 06 - React/Components-Workshop/client/src/components/Main/Posts/Post.js
@@ -1,23 +1,17 @@
 import style from './Posts.module.css';
 
-function Post() {
+function Post({key, description, author}) {
     return (
-        <>
+        <div key={key} className={style.post}>
             <img src="/blue-origami-bird.png" alt="Origami" />
-            <p className={style.description}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <p className={style.description}>{description}</p>
             <div className={style.postDiv}>
                 <span className={style.postSpan}>
                     <small>Author: </small>
-                    Some Anonimous
+                    {author}
                 </span>
             </div>
-        </>
+        </div>
     );
 }
 
--- 06 - React/Components-Workshop/client/src/components/Main/Posts/Posts.js
+++ 06 - React/Components-Workshop/client/src/components/Main/Posts/Posts.js
@@ -1,20 +1,11 @@
 import style from './Posts.module.css';
-
+import Post from './Post'
 function Posts({ posts }) {
     
     return (
         <div className={style.posts}>
             {posts.map(x => 
-                <div key={x.id}>
-                    <img src="/blue-origami-bird.png" alt="Origami" />
-                    <p className={style.description}>{x.description}</p>
-                    <div className={style.postDiv}>
-                        <span className={style.postSpan}>
-                            <small>Author: </small>
-                            {x.author}
-                        </span>
-                    </div>
-                </div>
+                <Post key = {x.id} description = {x.description} author = {x.author}/>
             )}
         </div>
     );
