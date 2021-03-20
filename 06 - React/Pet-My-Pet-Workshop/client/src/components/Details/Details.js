196b35d3928c8f12eaed0bb2d8adfd943c975e94
Iva Tosheva <otevi55@gmail.com>
2021-03-20 16:03:35 +0200

    added details page component

--- 06 - React/Pet-My-Pet-Workshop/client/src/components/Details/Details.js
+++ 06 - React/Pet-My-Pet-Workshop/client/src/components/Details/Details.js
@@ -1,5 +1,27 @@
-function Details() {
+import { useEffect, useState } from 'react';
 
+function Details({ match }) {
+    let [pet, setPet] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/pets/${match.params.id}`)
+            .then(res => res.json())
+            .then(res => setPet(res));
+    }, '')
+    console.log(pet);
+    
+    return (
+        <section className="detailsMyPet">
+            <h3>{pet.name}</h3>
+            <p>Pet counter: <i className="fas fa-heart"></i> {pet.likes}</p>
+            <p className="img"><img
+                src={pet.imageURL} /></p>
+            <form action="#" method="POST">
+                <textarea type="text" name="description">{pet.description}</textarea>
+                <button className="button"> Save</button>
+            </form>
+        </section>
+    )
 }
 
 export default Details;
